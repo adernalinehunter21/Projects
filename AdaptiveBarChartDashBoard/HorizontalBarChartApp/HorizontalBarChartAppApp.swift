a7daff9779791a956eaad7eb2eb81ff8acd1b7a0
VishwasNivarthi213 <webdeveloperservices243@gmail.com>
2022-12-02 17:57:15 +0530

    Committed Sales Chart App using Xcode+ Swift
    This Application has been created using Xcode and Swift technologies

---
+++ AdaptiveBarChartDashBoard/HorizontalBarChartApp/HorizontalBarChartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalBarChartAppApp.swift
+//  HorizontalBarChartApp
+//
+//  Created by Nivarti Jayaram on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalBarChartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
